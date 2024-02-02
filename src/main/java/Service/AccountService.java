00ea0f4bb48416e7ae732ec04b644bf4b8d7bd11
John Pinto <jwpinto2@gmail.com>
2024-02-02 01:40:12 +0000

    service calls for accountdao register and login

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,20 @@
 package Service;
 
+import Model.Account;
+import DAO.AccountDAO;
+
 public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
     
+    public Account register(Account account) {
+        return accountDAO.createAccount(account);
+    }
+
+    public Account login(Account account) {
+        return accountDAO.login(account);
+    }
 }
